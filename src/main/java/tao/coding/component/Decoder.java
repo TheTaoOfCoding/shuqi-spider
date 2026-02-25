8bf01fe0e6b3fefea954b0adb5f85163d82b10b6
zailushang <631271779@qq.com>
2026-02-25 14:42:26 +0800

    其安易持，其未兆易谋；其脆易泮，其微易散。为之于未有，治之于未乱。(处理快速失败)

--- src/main/java/tao/coding/component/Decoder.java
+++ src/main/java/tao/coding/component/Decoder.java
@@ -94,7 +94,7 @@ public interface Decoder extends Task<Chapter.Chapter4Decode, Chapter.Chapter4Fo
                     .whenCompleteAsync((_, _) -> log.info("{} - 执行解密操作", Decoder.name()), taskExecutor())
                     .thenApplyAsync(Chapter.Chapter4Decode::ciphertext, taskExecutor())
                     .whenCompleteAsync((ciphertext, _) -> Assert.isTrue(ciphertext, Assert::isNotNull, () -> new NullPointerException("无法下载VIP章节，如已开通VIP账号，请自行添加VIP权限校验。")), taskExecutor())
-                    .exceptionally(FailFastHandler::handle)
+                    .exceptionallyAsync(FailFastHandler::handle, taskExecutor())
                     .thenApplyAsync(FlowEngine.USE_NATIVE ? Decoder::withNativeDecode : Decoder::withJsDecode, taskExecutor())// 根据配置选择解密方式
                     .thenApplyAsync(unformattedChapterContent -> new Chapter.Chapter4Format(chapter4Decode.bookName(), chapter4Decode.chapterName(), chapter4Decode.chapterOrdid(), unformattedChapterContent), taskExecutor());
         }
