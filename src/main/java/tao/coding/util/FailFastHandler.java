be68379df63f764f2cdbc21e11899f9a54534e04
zailushang <631271779@qq.com>
2026-02-25 12:19:43 +0800

    其安易持，其未兆易谋；其脆易泮，其微易散。为之于未有，治之于未乱。(处理快速失败)

--- src/main/java/tao/coding/util/FailFastHandler.java
+++ src/main/java/tao/coding/util/FailFastHandler.java
@@ -1,12 +1,15 @@
 package tao.coding.util;
 
 import lombok.extern.slf4j.Slf4j;
+import tao.coding.flow.FlowEngine;
 
 @Slf4j
 public class FailFastHandler {
     public static <T> T handle(Throwable t) {
-        t.printStackTrace();
         log.error("\u001B[91m敕令：「心念不纯，符窍无光！僭请神明，触怒天罡！伏请三清垂慈，赦宥愚诚！」\u001B[0m");
+        FlowEngine.HTTP_CLIENT.close();
+        ScopedExecutor.shutdown();
+        t.printStackTrace();
         System.exit(1);
         return null;
     }
